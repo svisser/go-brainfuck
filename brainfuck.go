bae8b7889b6c50553c9e589443a03cf6030ee3cd
Simeon Visser <simeon87@gmail.com>
2014-12-29 23:34:01 +0100

    Refactored jumps into their own functions

--- brainfuck.go
+++ brainfuck.go
@@ -6,6 +6,42 @@ import "fmt"
 import "io/ioutil"
 import "os"
 
+func jump_forward(source []uint8, start int, source_length int) int {
+	var target = 0
+	var cnt = 1
+	for tip := start; tip < source_length; tip++ {
+		cc := string(source[tip])
+		if cc == "[" {
+			cnt += 1
+		} else if cc == "]" {
+			cnt -= 1
+		}
+		if cnt == 0 {
+			target = tip
+			break
+		}
+	}
+	return target + 1
+}
+
+func jump_backward(b []uint8, start int) int {
+	var target = 0
+	var cnt = 1
+	for tip := start; tip > 0; tip-- {
+		cc := string(b[tip])
+		if cc == "]" {
+			cnt += 1
+		} else if cc == "[" {
+			cnt -= 1
+		}
+		if cnt == 0 {
+			target = tip
+			break
+		}
+	}
+	return target + 1
+}
+
 func main() {
 	pathPtr := flag.String("path", "", "path to Brainfuck source")
 	sizePtr := flag.Int("size", 30000, "size of the data array")
@@ -17,7 +53,7 @@ func main() {
 		fmt.Println("You must specify a path to Brainfuck source: -path=...")
 	}
 
-	b, err := ioutil.ReadFile(path)
+	source, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
@@ -25,10 +61,10 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	var data = make([]int, *sizePtr)
-	var b_length = len(b)
+	var source_length = len(source)
 	var dp = 0
-	for ip := 0; ip < b_length; {
-		var c = string(b[ip])
+	for ip := 0; ip < source_length; {
+		var c = string(source[ip])
 		switch {
 		case c == ">":
 			dp += 1
@@ -57,41 +93,13 @@ func main() {
 			ip += 1
 		case c == "[":
 			if data[dp] == 0 {
-				var target = 0
-				var cnt = 1
-				for tip := (ip + 1); tip < b_length; tip++ {
-					cc := string(b[tip])
-					if cc == "[" {
-						cnt += 1
-					} else if cc == "]" {
-						cnt -= 1
-					}
-					if cnt == 0 {
-						target = tip
-						break
-					}
-				}
-				ip = target + 1
+				ip = jump_forward(source, ip+1, source_length)
 			} else {
 				ip += 1
 			}
 		case c == "]":
 			if data[dp] != 0 {
-				var target = 0
-				var cnt = 1
-				for tip := (ip - 1); tip > 0; tip-- {
-					cc := string(b[tip])
-					if cc == "]" {
-						cnt += 1
-					} else if cc == "[" {
-						cnt -= 1
-					}
-					if cnt == 0 {
-						target = tip
-						break
-					}
-				}
-				ip = target + 1
+				ip = jump_backward(source, ip-1)
 			} else {
 				ip += 1
 			}
